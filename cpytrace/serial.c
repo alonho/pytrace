ee024187bc73bdcf2ce1303be97aa5929ccc3c67
Alon Horev <alonho@gmail.com>
2012-05-28 09:50:26 +0300

    add forgotten seria.c file

---
+++ cpytrace/serial.c
@@ -0,0 +1,87 @@
+#include <pthread.h>
+#include "serial.h"
+#include "write.h"
+#include "defs.h"
+
+#define PYPRINT(obj) PyObject_Print(obj, stdout, Py_PRINT_RAW);
+
+static Record *record; // pre-allocate a single record to be re-used
+static Argument **arguments; // pre-allocate maximum number of arguments
+static unsigned char *record_buf;
+
+int init_serialize(void) {
+  record_buf = malloc(MAX_RECORD_SIZE);
+  record = malloc(sizeof(Record)); // TODO: check malloc retval
+  record__init(record);
+  arguments = malloc(sizeof(Argument*) * MAX_ARGS); // TODO: check malloc retval
+  record->arguments = arguments;
+  int i;
+  for (i = 0; i < MAX_ARGS; i++) {
+    arguments[i] = malloc(sizeof(Argument));
+    argument__init(arguments[i]);
+  }
+  return init_writer();
+}
+
+inline const char *pyobj_to_cstr(PyObject *obj) {
+  PyObject *string = PyObject_Repr(obj);
+  if (NULL == string) {
+    return "REPR FAILED";
+  }
+  return PyString_AsString(string);
+}
+
+inline static double floattime()
+{
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double) t.tv_sec + t.tv_usec * 0.000001;
+}
+
+void handle_trace(PyFrameObject *frame, Record__RecordType record_type, int n_arguments) 
+{
+  record->type = record_type;
+  record->n_arguments = n_arguments;
+  record->time = floattime();
+  record->tid = (unsigned int) pthread_self();
+  record->function = PyString_AsString(frame->f_code->co_name);
+  record->module = PyString_AsString(frame->f_code->co_filename);
+  record->depth = 0;
+  record__pack(record, record_buf);
+  write_record(record_buf, record__get_packed_size(record));
+}
+
+void handle_call(PyFrameObject *frame) {  
+  PyObject *name, *value;
+  int i;
+  for (i = 0; i < MIN(PyTuple_GET_SIZE(frame->f_code->co_varnames), MAX_ARGS); i++) {
+    name = PyTuple_GetItem(frame->f_code->co_varnames, i);
+    arguments[i]->name = PyString_AsString(name);
+    if (NULL == frame->f_locals) {
+      value = frame->f_localsplus[i];
+    } else {
+      value = PyDict_GetItem(frame->f_locals, name);
+    }
+    arguments[i]->type = pyobj_to_cstr(value->ob_type);
+    arguments[i]->value = pyobj_to_cstr(value);
+  }
+  handle_trace(frame, RECORD__RECORD_TYPE__CALL, i);
+}
+
+inline void handle_return(PyFrameObject *frame, PyObject *value) {
+  arguments[0]->name = "return_value";
+  if (NULL == value) {
+    value = Py_None;
+  }
+  arguments[0]->type = pyobj_to_cstr(value->ob_type);
+  arguments[0]->value = pyobj_to_cstr(value);
+  handle_trace(frame, RECORD__RECORD_TYPE__RETURN, 1);
+}
+    
+inline void handle_exception(PyFrameObject *frame, PyObject *exc_info) {
+  arguments[0]->name = "exception";
+  arguments[0]->type = pyobj_to_cstr(PyTuple_GET_ITEM(exc_info, 1));
+  arguments[0]->value = pyobj_to_cstr(PyTuple_GET_ITEM(exc_info, 2));
+  handle_trace(frame, RECORD__RECORD_TYPE__EXCEPTION, 1);
+}
+
