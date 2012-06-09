769b7e9341aa97ba8544569b37beb63ed0855835
Alon Horev <alonho@gmail.com>
2012-06-09 14:54:31 +0000

    add make script

---
+++ reader/main.py
@@ -0,0 +1,27 @@
+import urwid
+
+palette = [
+    ('banner', '', '', '', '#f9f', '#60d'),
+    ('streak', '', '', '', 'g50', '#404'),
+    ('inside', '', '', '', 'g38', '#808'),
+    ('outside', '', '', '', 'g27', '#a06'),
+    ('bg', '', '', '', 'g7', '#d06'),]
+
+txt = urwid.Text(('banner', u" Hello World "), align='center')
+map1 = urwid.AttrMap(txt, 'streak')
+pile = urwid.Pile([
+    urwid.AttrMap(urwid.Divider(), 'outside'),
+    urwid.AttrMap(urwid.Divider(), 'inside'),
+    map1,
+    urwid.AttrMap(urwid.Divider(), 'inside'),
+    urwid.AttrMap(urwid.Divider(), 'outside')])
+fill = urwid.Filler(pile)
+map2 = urwid.AttrMap(fill, 'bg')
+
+def exit_on_q(input):
+    if input in ('q', 'Q'):
+        raise urwid.ExitMainLoop()
+
+loop = urwid.MainLoop(map2, palette, unhandled_input=exit_on_q)
+loop.screen.set_terminal_properties(colors=256)
+loop.run()
